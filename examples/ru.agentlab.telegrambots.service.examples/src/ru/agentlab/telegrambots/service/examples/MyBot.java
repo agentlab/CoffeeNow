ac5b0a137cd4b13791c8a625a4653b08588d58a3
Alex Ivanov <amivanoff@gmail.com>
2017-03-28 22:26:17 +0300

    Refactor formatting in Telegram example

--- examples/ru.agentlab.telegrambots.service.examples/src/ru/agentlab/telegrambots/service/examples/MyBot.java
+++ examples/ru.agentlab.telegrambots.service.examples/src/ru/agentlab/telegrambots/service/examples/MyBot.java
@@ -19,57 +19,51 @@ import ru.telegram.telegrambots.bots.TelegramLongPollingBot;
  */
 @Component(service = LongPollingBot.class)
 public class MyBot extends TelegramLongPollingBot {
+	static Logger log = LoggerFactory.getLogger(MyBot.class);
 
-    static Logger log = LoggerFactory.getLogger(MyBot.class);
+	public static final String BOT_TOKEN = System.getProperty("MyBotToken", ""); //$NON-NLS-1$ //$NON-NLS-2$
+	public static final String BOT_USER_NAME = System.getProperty("MyBotUserName", ""); //$NON-NLS-1$ //$NON-NLS-2$
 
-    public static final String BOT_TOKEN = System.getProperty("MyBotToken", ""); //$NON-NLS-1$ //$NON-NLS-2$
-    public static final String BOT_USER_NAME = System.getProperty("MyBotUserName", ""); //$NON-NLS-1$ //$NON-NLS-2$
+	@Override
+	public String getBotUsername() {
+		return BOT_USER_NAME;
+	}
 
-    @Activate
-    public void activate() {
-        log.info("Activating component from {} class", MyBot.class.getName()); //$NON-NLS-1$
-    }
+	@Override
+	public String getBotToken() {
+		return BOT_TOKEN;
+	}
 
-    @Override
-    public String getBotUsername() {
-        return BOT_USER_NAME;
-    }
+	@Activate
+	public void activate() {
+		log.info("Activating component from {} class", MyBot.class.getName()); //$NON-NLS-1$
+	}
 
-    @Override
-    public void onUpdateReceived(Update update) {
-        //check if the update has a message
-        if (update.hasMessage())
-        {
-            Message message = update.getMessage();
+	@Override
+	public void onUpdateReceived(Update update) {
+		//check if the update has a message
+		if (update.hasMessage()) {
+			Message message = update.getMessage();
 
-            //check if the message has text. it could also  contain for example a location ( message.hasLocation() )
-            if (message.hasText())
-            {
+			//check if the message has text. it could also  contain for example a location ( message.hasLocation() )
+			if (message.hasText()) {
+				//create a object that contains the information to send back the message
+				SendMessage sendMessageRequest = new SendMessage();
+				//who should get the message? the sender from which we got the message...
+				sendMessageRequest.setChatId(message.getChatId().toString());
+				sendMessageRequest.setText("you said: " + message.getText()); //$NON-NLS-1$
+				try {
+					sendMessage(sendMessageRequest); //at the end, so some magic and send the message ;)
+				}
+				catch (TelegramApiException e) {
+					log.error("Unable to send message", e);
+				}
+			}
+		}
+	}
 
-                //create a object that contains the information to send back the message
-                SendMessage sendMessageRequest = new SendMessage();
-                //who should get the message? the sender from which we got the message...
-                sendMessageRequest.setChatId(message.getChatId().toString());
-                sendMessageRequest.setText("you said: " + message.getText()); //$NON-NLS-1$
-                try
-                {
-                    sendMessage(sendMessageRequest); //at the end, so some magic and send the message ;)
-                }
-                catch (TelegramApiException e)
-                {
-                    log.error("Unable to send message", e);
-                }
-            }
-        }
-    }
-
-    @Override
-    public String getBotToken() {
-        return BOT_TOKEN;
-    }
-
-    @Deactivate
-    public void deactivate() {
-        log.info("Deactivating component from {} class", MyBot.class.getName()); //$NON-NLS-1$
-    }
+	@Deactivate
+	public void deactivate() {
+		log.info("Deactivating component from {} class", MyBot.class.getName()); //$NON-NLS-1$
+	}
 }
