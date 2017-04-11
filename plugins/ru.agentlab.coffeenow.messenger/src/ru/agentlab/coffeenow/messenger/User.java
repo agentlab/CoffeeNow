735028e55c01313e3bf9a56b72bd9946717d2e9a
makci.97@mail.ru <makci.97@mail.ru>
2017-04-11 17:40:45 +0300

    Add class User

---
+++ plugins/ru.agentlab.coffeenow.messenger/src/ru/agentlab/coffeenow/messenger/User.java
@@ -0,0 +1,28 @@
+/**
+ *
+ */
+package ru.agentlab.coffeenow.messenger;
+
+/**
+ * @author maxim
+ *
+ */
+public class User {
+
+	private String id;
+
+	public User() {
+	}
+
+	public User(String id) {
+		this.id = id;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+}
