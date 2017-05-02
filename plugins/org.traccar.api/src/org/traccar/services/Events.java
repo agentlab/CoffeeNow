446250950d9119161ee09ff0c440e153e99718e9
Alex Ivanov <amivanoff@gmail.com>
2017-05-02 17:42:15 +0300

    22: Добавить Traccar API
    Task-Url: https://github.com/agentlab/CoffeeNow/issues/22

---
+++ plugins/org.traccar.api/src/org/traccar/services/Events.java
@@ -0,0 +1,9 @@
+package org.traccar.services;
+
+import org.traccar.model.Event;
+
+interface Events {
+
+    Event get(long id);
+
+}
---
+++ plugins/org.traccar.api/src/org/traccar/services/Groups.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2016 - 2017 Anton Tananaev (anton@traccar.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.traccar.services;
+
+import java.util.Collection;
+
+import org.traccar.model.Group;
+
+interface Groups {
+
+    Collection<Group> get(boolean all, long userId);
+
+    Group add(Group entity);
+
+    Group update(Group entity);
+
+    void remove(long id);
+
+}
