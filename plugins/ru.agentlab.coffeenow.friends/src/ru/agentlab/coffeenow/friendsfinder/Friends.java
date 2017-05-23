176e1fc25344dfb82d2d3fe101b31600ecc33007
Пилипенко Кирилл <kirich74@gmail.com>
2017-05-23 19:20:26 +0300

    Вроде всё =)

--- plugins/ru.agentlab.coffeenow.friends/src/ru/agentlab/coffeenow/friendsfinder/Friends.java
+++ plugins/ru.agentlab.coffeenow.friends/src/ru/agentlab/coffeenow/friendsfinder/Friends.java
@@ -5,7 +5,10 @@ package ru.agentlab.coffeenow.friendsfinder;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.traccar.model.Device;
@@ -19,22 +22,28 @@ import org.traccar.services.Positions;
  *
  */
 @Component()
-public class Friends {
+public class Friends extends TimerTask {
 
-	private Positions mMpositions;
+	private Positions positions;
 
-	private Devices mDevices;
+	private Devices devices;
 
 	private double RADIUS = 30;
 
+	Collection<Device> devicesCollection;
+
+	private int MYID = 0;
+
 	@Reference()
 	public void bindDevices(Devices devices) {
+		this.devices = devices;
 	}
 	public void unbindDevices(Devices devices) {
 	}
 
 	@Reference()
 	public void bindPosition(Positions position) {
+		this.positions = position;
 	}
 	public void unbindPosition(Positions position) {
 	}
@@ -44,32 +53,53 @@ public class Friends {
 	}
 
 	public Map<String, Long> getDevices(){
-		Collection<Device> devices = mDevices.getDevices();
+		devicesCollection = devices.getDevices();
 		Map<String, Long> result = new HashMap<String, Long>();
-		for (Device d : devices){
+		for (Device d : devicesCollection){
 			result.put(d.getName(), d.getId());
 		}
 		return result;
 	}
 
 	public void getPositions (double myId){
-		Collection<Position> positions = mMpositions.getPositions();
+		Collection<Position> positionsCollection = positions.getPositions();
+		getDevices();
 		double myX = 0, myY = 0;
-		for (Position p : positions){
+		for (Position p : positionsCollection){
 			if (p.getId() == myId){
 				myX = p.getAltitude();
 				myY = p.getLongitude();
 				break;
 			}
 		}
-		for (Position p : positions){
+		for (Position p : positionsCollection){
 			if (p.getId() != myId){
 				if (isFriendCloser(myX, p.getAltitude(), myY, p.getLongitude(), RADIUS)){
 					//do somethink
+					String name = "";
+					for (Device d : devicesCollection){
+						if (d.getId() == p .getId()){
+							name = d.getName();
+							break;
+						}
+					}
+					System.out.printf("friend with name ", name, " near you");
 				}
 			}
 		}
 
+	}
+
+    @Override
+    public void run() {
+    	getPositions(MYID);
+    }
+
+	@Activate
+	public void start(){
+		final TimerTask task = new Friends();
+		Timer timer = new Timer(true);
+        timer.scheduleAtFixedRate(task, 0, 10*1000);
 	}
 
 
